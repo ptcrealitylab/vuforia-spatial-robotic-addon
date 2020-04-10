ac202fd4113c9d1246533c7e3603363f9da9e226
Anna Fuste <afuste@ptc.com>
2020-04-10 14:45:24 -0400

    Removed myRobot example, refactored npm to be in root directory

--- interfaces/MIR100/index.js
+++ interfaces/MIR100/index.js
@@ -62,6 +62,8 @@ let enableMIRConnection = false;     // For debugging purposes, deactivate from 
 if (exports.enabled) {               // These settings will be exposed to the webFrontend to potentially be modified
 
     setup();
+
+    console.log('MIR: Settings loaded: ', objectName, hostIP, port, isRobotConnected, enableMIRConnection);
     
     function setup() {
         
@@ -114,10 +116,10 @@ if (exports.enabled) {               // These settings will be exposed to the we
     server.addEventListener('reset', function() {   // reload the settings from settings.json when you get a 'reset' message
         settings = server.loadHardwareInterface(__dirname);
         setup();
+
+        console.log('MIR: Settings loaded: ', objectName, hostIP, port, isRobotConnected, enableMIRConnection);
     });
 }
-
-console.log('MIR: Settings loaded: ', objectName, hostIP, port, isRobotConnected, enableMIRConnection);
 
 // Robot Websocket & REST variables
 let websocket, restapi, serverRest = null;
